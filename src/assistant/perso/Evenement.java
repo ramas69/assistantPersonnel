c1eaeb634fda15b92e4331558a7081e00369daf0
gk <gk@10.69.8.32>
2019-06-07 14:24:34 +0200

    creation classe evenement

--- src/assistant/perso/Evenement.java
+++ src/assistant/perso/Evenement.java
@@ -1,9 +1,47 @@
 package assistant.perso;
 
-public class Evenement {
+import java.util.Date;
 
+public class Evenement {
+	private int id; 
+	private String nomEvent;
+	private Date dateEvent;
+	
 	public Evenement() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Evenement(String nomEvent, Date dateEvent) {
+		super();
+		this.nomEvent = nomEvent;
+		this.dateEvent = dateEvent;
+	}
+
+	public String getNomEvent() {
+		return nomEvent;
+	}
+
+	public void setNomEvent(String nomEvent) {
+		this.nomEvent = nomEvent;
+	}
+
+	public Date getDateEvent() {
+		return dateEvent;
+	}
+
+	public void setDateEvent(Date dateEvent) {
+		this.dateEvent = dateEvent;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
 
 }
